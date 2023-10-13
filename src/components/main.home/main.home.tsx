c05979806962880a6c7cb75f0e79f4565c323b8e
Sergio G. Girona <sergioglez_8@hotmail.com>
2023-10-13 12:34:30 +0200

    Add image to home for desktop viewers

--- src/components/main.home/main.home.tsx
+++ src/components/main.home/main.home.tsx
@@ -3,6 +3,9 @@ export function Home() {
   return (
     <div className={styles['main']}>
       <video autoPlay loop muted src="../../../lobo_video.mp4"></video>
+
+      <img src="../../../fondo_wolftracks.png" />
+
       <div>
         <h2>
           Huellas que <em>inspiran acción</em>
